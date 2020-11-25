f3a13b4cfe78511b9e093239c5ce456ff898236d
dithwing76 <72815371+dithwing76@users.noreply.github.com>
2020-11-25 17:30:24 +0000

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,13 @@
+class Ball extends BaseClass {
+  constructor(x,y){
+    super(x,y,30,30);
+    
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+    fill("black");
+  }
+}
